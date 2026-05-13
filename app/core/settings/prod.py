0b3137cf6e19385dc0bcc00a89b58302d1df020e
Nurbolottop <erk1nbaevw2711@gmail.com>
2026-05-13 12:01:19 +0600

    chore: prepare docker config for production deployment on greenenergy.su

--- app/core/settings/prod.py
+++ app/core/settings/prod.py
@@ -3,3 +3,12 @@ from core.settings.base import *
 DEBUG = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = ["greenenergy.su", "www.greenenergy.su"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://greenenergy.su",
+    "https://www.greenenergy.su",
+]
+
+STATIC_ROOT = "/app/staticfiles"
